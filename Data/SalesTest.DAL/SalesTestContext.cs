e9f56ba4bc0926657ff59518a0d6c32720b1644c
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-06 22:12:06 +0300

    установка нугет пакетов для БД ин-мемори. Создание контекста

---
+++ Data/SalesTest.DAL/SalesTestContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesTest.DAL
+{
+    public class SalesTestContext : DbContext
+    {
+        public SalesTestContext(DbContextOptions<SalesTestContext> options) : base(options)
+        {
+
+        }
+
+        // TODO DBSets
+    }
+}
