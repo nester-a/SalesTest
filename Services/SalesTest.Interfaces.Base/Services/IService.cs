86bb98b283903635992eeff2ff83be38c32829be
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-06 22:21:29 +0300

    добавлены базовые сервисы и репозиторий

---
+++ Services/SalesTest.Interfaces.Base/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using SalesTest.Domain.Base;
+using System.Collections.Generic;
+
+namespace SalesTest.Interfaces.Base.Services
+{
+    public interface IService<T> where T : IEntity
+    {
+        int Add(T item);
+
+        int Update(int id, T updatedItem);
+
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+
+        T Delete(int id);
+    }
+}
