804477f484f7988be0fd512781e604261a6fcced
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-06 21:55:21 +0300

    интерфейсы основных сущностей

---
+++ Common/SalesTest.Domain.Base/IBuyer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SalesTest.Domain.Base
+{
+    /// <summary>Buyer</summary>
+    public interface IBuyer : INamedEntity
+    {
+        /// <summary>Collection of all buyer purchases</summary>
+        ICollection<int> SalesIds { get; set; }
+    }
+}
