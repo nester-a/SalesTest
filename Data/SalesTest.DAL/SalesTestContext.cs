f9fdbc1e23e663ba5d50bca64200f6e365fbf368
Aleksey N <71524040+nester-a@users.noreply.github.com>
2022-06-07 17:38:09 +0300

    в DAL добавлен метод OnModelCreating, в него добавлена конфигурация моделей. Теперь нужно только настроить сами конфигурации моделей и переместить их в отдельные классы

--- Data/SalesTest.DAL/SalesTestContext.cs
+++ Data/SalesTest.DAL/SalesTestContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SalesTest.DAL.Enities;
 
 namespace SalesTest.DAL
@@ -15,5 +16,64 @@ namespace SalesTest.DAL
         public DbSet<Sales> Sales { get; set; }
         public DbSet<SalesData> SalesData { get; set; }
         public DbSet<SalesPoint> SalesPoints { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new BuyerConfiguration());
+            builder.ApplyConfiguration(new ProductConfiguration());
+            builder.ApplyConfiguration(new ProvidedProductConfiguration());
+            builder.ApplyConfiguration(new SalesConfiguration());
+            builder.ApplyConfiguration(new SalesDataConfiguration());
+            builder.ApplyConfiguration(new SalesPointsConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+    
+    public class BuyerConfiguration : IEntityTypeConfiguration<Buyer>
+    {
+        public void Configure(EntityTypeBuilder<Buyer> builder>)
+        {
+            // todo
+        }
+    }
+
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            // todo
+        }
+    }
+
+    public class ProvidedProductConfiguration : IEntityTypeConfiguration<ProvidedProduct>
+    {
+        public void Configure(EntityTypeBuilder<ProvidedProduct> builder)
+        {
+            // todo
+        }
+    }
+
+    public class SalesConfiguration : IEntityTypeConfiguration<Sales>
+    {
+        public void Configure(EntityTypeBuilder<Sales> builder)
+        {
+            // todo
+        }
+    }
+
+    public class SalesDataConfiguration : IEntityTypeConfiguration<SalesData>
+    {
+        public void Configure(EntityTypeBuilder<SalesData> builder)
+        {
+            // todo
+        }
+    }
+
+    public class SalesPointsConfiguration : IEntityTypeConfiguration<SalesPoints>
+    {
+        public void Configure(EntityTypeBuilder<SalesPoints> builder)
+        {
+            // todo
+        }
     }
 }
