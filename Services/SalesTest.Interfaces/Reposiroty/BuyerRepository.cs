e7d8e901ccceeb2d47a23d477c7c14adebe11382
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-08 22:13:15 +0300

    добавлен и оттестирован метод апдайт

--- Services/SalesTest.Interfaces/Reposiroty/BuyerRepository.cs
+++ Services/SalesTest.Interfaces/Reposiroty/BuyerRepository.cs
@@ -2,10 +2,18 @@ using SalesTest.Interfaces.Base.Repository;
 using SalesTest.DAL;
 using SalesTest.Domain;
 using System.Collections.Generic;
+using SalesTest.Interfaces.Extensions;
+
+using BuyerDAL = SalesTest.DAL.Enities.Buyer;
+using SalesDAL = SalesTest.DAL.Enities.Sales;
+using BuyerDOM = SalesTest.Domain.Buyer;
+using SalesDOM = SalesTest.Domain.Sales;
+using System;
+using System.Linq;
 
 namespace SalesTest.SalesTest.Interfaces.Repository
 {
-    public class BuyerRepository : IRepository<Buyer>
+    public class BuyerRepository : IRepository<BuyerDOM>
     {
         SalesTestContext _context;
         public BuyerRepository(SalesTestContext context)
@@ -13,27 +21,42 @@ namespace SalesTest.SalesTest.Interfaces.Repository
             _context = context;
         }
 
-        public int Add(Buyer item)
+        public int Add(BuyerDOM item)
+        {
+            if (item == null) throw new ArgumentNullException("Item is null");
+            var result = MapBuyerToDal(item);
+
+            return _context.Buyers.Add(result).Entity.Id;
+        }
+
+        public int Update(int id, BuyerDOM updatedItem)
+        {
+            if (updatedItem == null) throw new ArgumentNullException("Item is null");
+
+            var exsist = _context.Buyers.FirstOrDefault(i => i.Id == id);
+            if(exsist is null) throw new ArgumentException("Item not found");
+
+            exsist.Name = updatedItem.Name;
+            exsist.Sales = GetSales(updatedItem.SalesIds);
+
+            _context.Buyers.Update(exsist);
+
+            return id;
+
+        }
+
+        public List<BuyerDOM> GetAll()
+        {
+            var all = _context.Buyers.ToList();
+            return all.Select(i => i.ToDal()).ToList();
+        }
+
+        public BuyerDOM GetById(int id)
         {
             return default;
         }
 
-        public int Update(int id, Buyer updatedItem)
-        {
-            return default;
-        }
-
-        public IEnumerable<Buyer> GetAll()
-        {
-            return default;
-        }
-
-        public Buyer GetById(int id)
-        {
-            return default;
-        }
-
-        public Buyer Delete(int id)
+        public BuyerDOM Delete(int id)
         {
             return default;
         }
@@ -42,6 +65,29 @@ namespace SalesTest.SalesTest.Interfaces.Repository
         {
             _context.SaveChanges();
         }
+
+        private BuyerDAL MapBuyerToDal(BuyerDOM item)
+        {
+            var result = item.ToDAL();
+            List<SalesDAL> sales = GetSales(item.SalesIds);
+
+            result.Sales = sales;
+            return result;
+        }
+
+        private List<SalesDAL> GetSales(List<int> ids)
+        {
+            List<SalesDAL> sales = new List<SalesDAL>();
+            if (ids.Count == 0) return sales;
+            foreach (var sale in ids)
+            {
+                var findedSale = _context.Sales.Find(sale);
+                sales.Add(findedSale);
+            }
+
+            return sales;
+        }
+
     }
 
 }
---
+++ Tests/TestProject/Services/ServiceTestInterfaces/Repository/BuyerRepositoryTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesTest.DAL;
+using SalesTest.Domain;
+using SalesTest.SalesTest.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject.Services.ServiceTestInterfaces.Repository
+{
+    public class BuyerRepositoryTests
+    {
+        static DbContextOptionsBuilder<SalesTestContext> optionBuilder = new DbContextOptionsBuilder<SalesTestContext>();
+        static DbContextOptions<SalesTestContext> option = optionBuilder.UseInMemoryDatabase("TestDB").Options;
+        static SalesTestContext db = new SalesTestContext(option);
+
+        static BuyerRepositoryTests()
+        {
+            DbInitializer.Initializer(db);
+        }
+
+        [Fact]
+        public void BuyerRepository_Add_Test()
+        {
+            BuyerRepository repo = new BuyerRepository(db);
+
+            var buyer = new Buyer()
+            {
+                Name = "Hello",
+            };
+
+            var result = repo.Add(buyer);
+            repo.Save();
+
+            Assert.True(result == 1);
+            Assert.True(db.Buyers.Find(result).Name == buyer.Name);
+        }
+
+        [Fact]
+        public void BuyerRepository_Update_Test()
+        {
+            BuyerRepository repo = new BuyerRepository(db);
+
+            var buyer = new Buyer()
+            {
+                Name = "Hello",
+            };
+
+            var added = repo.Add(buyer);
+            repo.Save();
+
+            Assert.True(added == 1);
+            var buyerDal = db.Buyers.Find(added);
+
+            Assert.True(buyerDal.Id == 1);
+            Assert.True(buyerDal.Name == buyer.Name);
+
+            buyer.Id = buyerDal.Id;
+            buyer.Name = "Hello World";
+
+            var updated = repo.Update(buyer.Id, buyer);
+            repo.Save();
+
+            Assert.True(updated == 1);
+            buyerDal = db.Buyers.Find(updated);
+
+            Assert.True(buyerDal.Name == buyer.Name);
+        }
+
+        [Fact]
+        public void BuyerRepository_GetAll_Test()
+        {
+            BuyerRepository repo = new BuyerRepository(db);
+
+            var buyer1 = new Buyer()
+            {
+                Name = "Hello",
+            };
+            var buyer2 = new Buyer()
+            {
+                Name = "World",
+            };
+
+            repo.Add(buyer1);
+            repo.Add(buyer2);
+            repo.Save();
+
+            Assert.True(db.Buyers.Count() == 2);
+
+            var result = repo.GetAll();
+            Assert.Equal(2, result.Count);
+
+            var entity1 = result.FirstOrDefault(e => e.Name == buyer1.Name);
+            Assert.True(entity1 is not null);
+            Assert.Equal(entity1.Name, buyer1.Name);
+
+            var entity2 = result.FirstOrDefault(e => e.Name == buyer2.Name);
+            Assert.True(entity2 is not null);
+            Assert.Equal(entity2.Name, buyer2.Name);
+        }
+    }
+}
