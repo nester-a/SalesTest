8b25ccf4002a0eb4f206cd38ef2f9d9520fd2388
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-08 22:42:16 +0300

    добавление метод эдд и его тест

---
+++ Tests/TestProject/Services/ServiceTestInterfaces/Repository/ProductsRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using SalesTest.Domain;
+using SalesTest.SalesTest.Interfaces.Repository;
+using System.Linq;
+using Xunit;
+
+namespace TestProject.Services.ServiceTestInterfaces.Repository
+{
+    public class ProductsRepositoryTests : BaseTest
+    {
+        [Fact]
+        public void BuyerRepository_Add_Test()
+        {
+            ProductRepository repo = new ProductRepository(db);
+
+            var product = new Product()
+            {
+                Name = "Hello",
+                Price = 15.12m,
+            };
+
+            var result = repo.Add(product);
+            repo.Save();
+
+            Assert.True(result == 1);
+            Assert.True(db.Products.Find(result).Name == product.Name);
+            Assert.True(db.Products.Find(result).Price == product.Price);
+        }
+    }
+}
