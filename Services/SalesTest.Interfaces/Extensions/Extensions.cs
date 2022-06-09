830016441d4f86bb692d2494cdfffe5013f24953
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-09 21:07:05 +0300

    методы расширения для маппинга

--- Services/SalesTest.Interfaces/Extensions/Extensions.cs
+++ Services/SalesTest.Interfaces/Extensions/Extensions.cs
@@ -1,8 +1,13 @@
 ﻿using BuyerDAL = SalesTest.DAL.Enities.Buyer;
 using ProductDAL = SalesTest.DAL.Enities.Product;
+using SalesPointDAL = SalesTest.DAL.Enities.SalesPoint;
+using ProvidedProductDAL = SalesTest.DAL.Enities.ProvidedProduct;
 using BuyerDOM = SalesTest.Domain.Buyer;
 using ProductDOM = SalesTest.Domain.Product;
+using SalesPointDOM = SalesTest.Domain.SalesPoint;
+using ProvidedProductDOM = SalesTest.Domain.ProvidedProduct;
 using System.Linq;
+using SalesTest.Domain.Base;
 
 namespace SalesTest.Interfaces.Extensions
 {
@@ -49,6 +54,54 @@ namespace SalesTest.Interfaces.Extensions
                 Id = item.Id,
                 Name = item.Name,
                 Price = item.Price,
+            };
+        }
+        public static ProvidedProductDAL ToDAL(this ProvidedProductDOM item)
+        {
+            return new ProvidedProductDAL()
+            {
+                ProductId = item.ProductId,
+                ProductQuantity = item.ProductQuantity,
+
+                // наполнение коллекции Sales происходит непосредственно в репозитории
+            };
+        }
+        public static ProvidedProductDAL ToDAL(this IProvidedProduct item)
+        {
+            return new ProvidedProductDAL()
+            {
+                ProductId = item.ProductId,
+                ProductQuantity = item.ProductQuantity,
+
+                // наполнение коллекции Sales происходит непосредственно в репозитории
+            };
+        }
+        public static IProvidedProduct ToDOM(this ProvidedProductDAL item)
+        {
+            return new ProvidedProductDOM()
+            {
+                ProductId = item.ProductId,
+                ProductQuantity = item.ProductQuantity,
+            };
+        }
+
+        public static SalesPointDAL ToDAL(this SalesPointDOM item)
+        {
+            return new SalesPointDAL()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                ProvidedProducts = item.ProvidedProducts.Select(i => i.ToDAL()).ToList(),
+            };
+        }
+
+        public static SalesPointDOM ToDOM(this SalesPointDAL item)
+        {
+            return new SalesPointDOM()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                ProvidedProducts = item.ProvidedProducts.Select(i => i.ToDOM()).ToList(),
             };
         }
     }
