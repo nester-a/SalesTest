93f6891b43ca3b53d3a10e52da226db0eaaa2f60
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-09 23:01:20 +0300

    метод адд и его тест

--- Services/SalesTest.Interfaces/Reposiroty/SalesRepository.cs
+++ Services/SalesTest.Interfaces/Reposiroty/SalesRepository.cs
@@ -2,6 +2,16 @@ using SalesTest.Interfaces.Base.Repository;
 using SalesTest.DAL;
 using SalesTest.Domain;
 using System.Collections.Generic;
+using System;
+using SalesTest.Interfaces.Extensions;
+
+using BuyerDAL = SalesTest.DAL.Enities.Buyer;
+using SalesDAL = SalesTest.DAL.Enities.Sales;
+using SalesDataDAL = SalesTest.DAL.Enities.SalesData;
+using BuyerDOM = SalesTest.Domain.Buyer;
+using SalesDataDOM = SalesTest.Domain.SalesData;
+using System.Linq;
+using SalesTest.Domain.Base;
 
 namespace SalesTest.SalesTest.Interfaces.Repository
 {
@@ -15,7 +25,23 @@ namespace SalesTest.SalesTest.Interfaces.Repository
 
         public int Add(Sales item)
         {
-            return default;
+            if (item == null) throw new ArgumentNullException("Item is null");
+
+
+            //var list = item.SalesData.Select(i => MapSalesDataToDal(i, item)).ToList();
+
+
+
+            var result = MapSalesToDal(item);
+            result.SalesData = item.SalesData.Select(i => MapSalesDataToDal(i, item)).ToList();
+            var id = _context.Sales.Add(result).Entity.Id;
+
+            //_context.SaveChanges();
+            //var res = _context.Sales.FirstOrDefault(i => i.Id == id);
+            //var sd = _context.SalesData.ToList();
+
+
+            return id;
         }
 
         public int Update(int id, Sales updatedItem)
@@ -42,6 +68,21 @@ namespace SalesTest.SalesTest.Interfaces.Repository
         {
             _context.SaveChanges();
         }
-    }
 
+        private SalesDAL MapSalesToDal(Sales item)
+        {
+            var result = item.ToDAL();
+            result.Buyer = _context.Buyers.FirstOrDefault(i => i.Id == item.Id);
+
+            return result;
+        }
+
+        private SalesDataDAL MapSalesDataToDal(ISalesData salesData, Sales item)
+        {
+            var result = salesData.ToDAL();
+            result.SalesId = item.Id;
+
+            return result;
+        }
+    }
 }
---
+++ Tests/TestProject/Services/ServiceTestInterfaces/Repository/SalesRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using SalesTest.Domain;
+using SalesTest.Domain.Base;
+using SalesTest.SalesTest.Interfaces.Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Xunit;
+
+namespace TestProject.Services.ServiceTestInterfaces.Repository
+{
+    public class SalesRepositoryTests : BaseTest
+    {
+        static SalesRepository repo = new SalesRepository(db);
+        [Fact]
+        public void SalesRepository_Add_Test()
+        {
+            var list = new List<ISalesData>()
+            {
+                new SalesData()
+                {
+                    ProductId = 1,
+                    ProductIdAmount = 1,
+                    ProductQuantity = 1,
+                },
+                new SalesData()
+                {
+                    ProductId = 2,
+                    ProductIdAmount = 2,
+                    ProductQuantity = 2,
+                },
+            };
+
+            Sales sales = new Sales()
+            {
+                SalesPointId = 1,
+                SalesData = list,
+                TotalAmount = list.Select(i => i.ProductIdAmount).Sum(),
+            };
+
+            var result = repo.Add(sales);
+            repo.Save();
+
+            Assert.True(result == 1);
+            Assert.True(db.Sales.Find(result).SalesPointId == sales.SalesPointId);
+            Assert.True(db.Sales.Find(result).DateTime == DateTimeOffset.Parse(sales.Date + " " + sales.Time));
+            Assert.True(db.Sales.Find(result).Buyer is null);
+            Assert.True(db.Sales.Find(result).BuyerId is null);
+            Assert.True(db.Sales.Find(result).SalesData.Count == 2);
+            Assert.True(db.SalesData.ToList().Count == 2);
+            
+
+        }
+    }
+}
