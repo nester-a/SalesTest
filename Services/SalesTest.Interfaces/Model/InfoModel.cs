df6df06f4e258a897a20e187611bcfbd24b5708a
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-10 22:17:25 +0300

    модель передачи всё информации из репозиториев

---
+++ Services/SalesTest.Interfaces/Model/InfoModel.cs
@@ -0,0 +1,14 @@
+﻿using SalesTest.Domain.Base;
+using SalesTest.Interfaces.Base.Model;
+using System.Collections.Generic;
+
+namespace SalesTest.Interfaces.Model
+{
+    public class InfoModel : IInfoModel
+    {
+        public List<IBuyer> BuyersInfo { get; set; }
+        public List<ISales> SalesInfo { get; set; }
+        public List<ISalesPoint> SalesPointsInfo { get; set; }
+        public List<IProduct> ProductInfo { get; set; }
+    }
+}
