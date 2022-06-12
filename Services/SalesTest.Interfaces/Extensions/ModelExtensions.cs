96574215b58c88b67fe466789f3a1d4958007198
Aleksey N <nester.a.l@yandex.ru>
2022-06-12 22:08:04 +0300

    модели для контроллера товаров

---
+++ Services/SalesTest.Interfaces/Extensions/ModelExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SalesTest.Domain.Base;
+using SalesTest.Interfaces.Model.Product;
+
+namespace SalesTest.Interfaces.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static ProductModel ToModel(this IProduct item)
+        {
+            return new ProductModel()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+            };
+        }
+    }
+}
