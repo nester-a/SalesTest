36cdded8c552bda4079c7c3c35692fade7207db7
Aleksey N <nester.a.l@yandex.ru>
2022-06-12 23:01:16 +0300

    модели точки продаж

---
+++ Services/SalesTest.Interfaces/Model/SalesPoint/CreateSalesPointModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTest.Interfaces.Model.SalesPoint
+{
+    public class CreateSalesPointModel
+    {
+
+        public string Name { get; set; }
+
+        public List<ProvidedProductModel> ProvidedProducts { get; set; } = new List<ProvidedProductModel>();
+    }
+}
---
+++ Services/SalesTest.Interfaces/Model/SalesPoint/ProvidedProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTest.Interfaces.Model.SalesPoint
+{
+    public class ProvidedProductModel
+    {
+        public int ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
