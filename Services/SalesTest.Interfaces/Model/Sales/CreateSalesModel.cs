c1fc0c51c40f37b56eec9d86acda817d96cf4571
Aleksey N <nester.a.l@yandex.ru>
2022-06-12 23:53:17 +0300

    модели для салес

---
+++ Services/SalesTest.Interfaces/Model/Sales/CreateSalesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SalesTest.Interfaces.Model.Sales
+{
+    public class CreateSalesModel
+    {
+        public int SalesPointId { get; set; }
+
+        public int? BuyerId { get; set; }
+
+        public List<SalesDataModel> SalesData { get; set; } = new List<SalesDataModel>();
+
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ Services/SalesTest.Interfaces/Model/Sales/SalesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesTest.Interfaces.Model.Sales
+{
+    public class SalesModel
+    {
+        public int Id { get; set; }
+
+        public string Date { get; set; } = DateTime.Now.Date.ToShortDateString();
+
+        public string Time { get; set; } = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+
+        public int SalesPointId { get; set; }
+
+        public int? BuyerId { get; set; }
+
+        public List<SalesDataModel> SalesData { get; set; } = new List<SalesDataModel>();
+
+        public decimal TotalAmount { get; set; }
+    }
+}
